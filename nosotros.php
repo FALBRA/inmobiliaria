d1c969899dd7a8e51bad36a007bc85611215055b
tangyrocket <adim180716@gmail.com>
2022-08-06 23:03:39 -0500

    header and footer
    Footer y heather agregados
    Migrado a PHP
    Creada el archivo proyectos.php
    A EVALUAR!

---
+++ nosotros.php
@@ -0,0 +1,144 @@
+<?php   include('header.php'); ?>
+
+    <!-- Header End -->
+    <div class="container-xxl py-5 bg-dark page-header mb-5">
+      <div class="container my-5 pt-5 pb-4">
+          <h1 class="display-3 text-white mb-3 animated slideInDown text-uppercase"><b>Nosotros</b></h1>
+          <nav aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                  <li class="breadcrumb-item"><a href="index.html">Inicio</a></li>
+                  <li class="breadcrumb-item"><a href="">Proyectos</a></li>
+                  <li class="breadcrumb-item text-white active" aria-current="page">Contactanos</li>
+              </ol>
+          </nav>
+      </div>
+  </div>
+  <!-- Header End -->
+
+  <!-- About Start -->
+  <div class="container-xxl py-5">
+    <div class="container">
+      <div class="row g-5">
+        <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+          <div
+            class="position-relative overflow-hidden ps-5 pt-5 h-100"
+            style="min-height: 400px"
+          >
+            <img
+              class="position-absolute w-100 h-100"
+              src="img/nosotros2.jpg"
+              alt=""
+              style="object-fit: cover"
+            />
+            <div
+              class="position-absolute top-0 start-0 bg-white pe-3 pb-3"
+              style="width: 250px; height: 200px"
+            >
+              <div
+                class="d-flex flex-column justify-content-center text-center bg-primary h-100 p-3"
+              >
+                <h1 class="text-white">Nosotros</h1>
+                
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.5s">
+          <div class="h-100">
+            <div class="border-start border-5 border-primary ps-4 mb-5">
+              <h1 class="display-6 mb-0">
+                Unica solucion para buscar los mejores Proyectos Inmobiliarios
+              </h1>
+            </div>
+            <p>
+              Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit.
+              Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit,
+              sed stet lorem sit clita duo justo magna dolore erat amet
+            </p>
+            <p class="mb-4">
+              Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit.
+              Aliqu diam amet diam et eos. Clita erat ipsum et lorem et sit,
+              sed stet lorem sit clita duo justo magna dolore erat amet
+            </p>
+            <div class="border-top mt-4 pt-4">
+              <div class="row g-4">
+                <div class="col-sm-4 d-flex wow fadeIn" data-wow-delay="0.1s">
+                  <i
+                    class="fa fa-check fa-2x text-primary flex-shrink-0 me-3"
+                  ></i>
+                  <h6 class="mb-0">Ontime at services</h6>
+                </div>
+                <div class="col-sm-4 d-flex wow fadeIn" data-wow-delay="0.3s">
+                  <i
+                    class="fa fa-check fa-2x text-primary flex-shrink-0 me-3"
+                  ></i>
+                  <h6 class="mb-0">24/7 hours services</h6>
+                </div>
+                <div class="col-sm-4 d-flex wow fadeIn" data-wow-delay="0.5s">
+                  <i
+                    class="fa fa-check fa-2x text-primary flex-shrink-0 me-3"
+                  ></i>
+                  <h6 class="mb-0">Verified professionals</h6>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- About End -->
+
+  <!-- Team Start -->
+  <div class="container-xxl py-5">
+    <div class="container">
+      <div class="row g-5 align-items-end mb-5">
+        <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.1s">
+          <div class="border-start border-5 border-primary ps-4">
+            <h6 class="text-body text-uppercase mb-2">Nuestro Equipo</h6>
+            <h1 class="display-6 mb-0"><b>Nuestros Trabajadores Expertos</b></h1>
+          </div>
+        </div>
+        <div class="col-lg-6 wow fadeInUp" data-wow-delay="0.3s">
+          <p class="mb-0">
+            Tempor erat elitr rebum at clita. Diam dolor diam ipsum sit. Aliqu
+            diam amet diam et eos. Clita erat ipsum et lorem et sit, sed stet
+            lorem sit clita duo justo magna dolore erat amet
+          </p>
+        </div>
+      </div>
+      <div class="row g-4">
+        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
+          <div class="team-item position-relative">
+            <img class="img-fluid" src="img/persona.jpg" alt="" />
+            <div class="team-text bg-white p-4">
+              <h5>Nombre completo</h5>
+              <span>profesion</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.3s">
+          <div class="team-item position-relative">
+            <img class="img-fluid" src="img/persona.jpg" alt="" />
+            <div class="team-text bg-white p-4">
+              <h5>Nombre completo</h5>
+              <span>profesion</span>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-6 wow fadeInUp" data-wow-delay="0.5s">
+          <div class="team-item position-relative">
+            <img class="img-fluid" src="img/persona.jpg" alt="" />
+            <div class="team-text bg-white p-4">
+              <h5>Nombre completo</h5>
+              <span>profesion</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Team End -->
+
+
+  <?php   include('footer.php'); ?>
